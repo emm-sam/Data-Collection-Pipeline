497ded10314185ead23ae902266144cb6ed84e33
Emma Samouelle <emma.samouelle@gmail.com>
2022-05-25 20:20:12 +0100

    Tests website search

--- testing/test_project.py
+++ testing/test_project.py
@@ -7,6 +7,7 @@ class ScraperTestCase(unittest.TestCase):
         print('setUpClass')
         cls.instance = PerfumeScraper("https://bloomperfume.co.uk/collections/perfumes")
         cls.url = "https://bloomperfume.co.uk/collections/perfumes"
+        cls.stem = "https://bloomperfume.co.uk"
         pass
 
     @classmethod
@@ -17,6 +18,14 @@ class ScraperTestCase(unittest.TestCase):
     def test_open_webpage(self):
         test_value = self.url
         self.instance.open_webpage(test_value)
+        actual_value = str(self.instance.get_current_url())
+        self.assertMultiLineEqual(test_value, actual_value)
+        pass
+
+    def test_search_website(self):
+        test_value = self.stem + "/search?q=product"
+        string = 'product'
+        self.instance.search_website(string)
         actual_value = str(self.instance.get_current_url())
         self.assertMultiLineEqual(test_value, actual_value)
         pass
