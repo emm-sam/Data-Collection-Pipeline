ef23967a3399c6becc30df82cba1bf77f66bdfad
Emma Samouelle <emma.samouelle@gmail.com>
2022-06-28 19:21:34 +0100

    Updates yaml

--- webscraper_project/scraper.py
+++ webscraper_project/scraper.py
@@ -25,7 +25,7 @@ import yaml
 
 class PerfumeScraper: 
    
-    def __init__(self, url:str, creds: str='config/RDS_creds.yaml'):
+    def __init__(self, url:str, creds: str='creds/rds_creds.yaml'):
         # options = webdriver.ChromeOptions() 
         options = Options()
         # options = ChromeOptions()
